ac18d750c452465bd566813635695d8ec2d120ae
buju <25bwang@athenian.org>
2023-05-08 14:34:47 -0700

    Done

---
+++ src/FrontPage.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FrontPage implements ActionListener {
+    JFrame frame;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public FrontPage(){
+        frame = new JFrame();
+    }
+}

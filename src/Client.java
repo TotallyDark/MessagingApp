e675d02254c0979bb7767d03b5e51fe7732ad601
TotallyDark <25wnie@athenain.org>
2023-05-09 09:18:55 -0700

    Test3

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,5 @@
 // A Java program for a Client
+/*
 import java.io.*;
 import java.net.*;
 
@@ -59,6 +60,52 @@ public class Client {
 
     public static void main(String args[])
     {
-        Client client = new Client("10.187.0.6", 5000);
+        Client client = new Client("10.16.108.67", 50000);
+    }
+}
+
+
+// Java program to illustrate Client side
+// Implementation using DatagramSocket
+*/
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String args[]) throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+
+        // Step 1:Create the socket object for
+        // carrying the data.
+        DatagramSocket ds = new DatagramSocket();
+
+        InetAddress ip = InetAddress.getLocalHost();
+        byte buf[] = null;
+
+        // loop while user not enters "bye"
+        while (true)
+        {
+            String inp = sc.nextLine();
+
+            // convert the String input into the byte array.
+            buf = inp.getBytes();
+
+            // Step 2 : Create the datagramPacket for sending
+            // the data.
+            DatagramPacket DpSend =
+                    new DatagramPacket(buf, buf.length, ip, 1234);
+
+            // Step 3 : invoke the send call to actually send
+            // the data.
+            ds.send(DpSend);
+
+            // break the loop if user enters "bye"
+            if (inp.equals("bye"))
+                break;
+        }
     }
 }

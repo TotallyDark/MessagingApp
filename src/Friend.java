ee9d6eef2ed3abc718122ce651363990eb1f5c34
buju <25bwang@athenian.org>
2023-05-10 10:17:42 -0700

    Buttons

---
+++ src/Friend.java
@@ -0,0 +1,14 @@
+public class Friend {
+    private String name;
+
+    public Friend(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public String setName(String name){
+        this.name = name;
+        return name;
+    }
+}

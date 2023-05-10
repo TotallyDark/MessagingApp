08e2be0cfcdfbbca10cc87eca9100c687ecc2434
Jerry <25rchen@athenian.org>
2023-05-10 10:39:55 -0700

    Still Working on the PersonalInfo Page

--- src/PersonalInfo.java
+++ src/PersonalInfo.java
@@ -1,12 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class PersonalInfo implements ActionListener{
+    JFrame frame1;
+    JPanel ButtonPanel;
+    JButton NameSet, ProfileSet;
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
     public PersonalInfo() {
+        frame1 = new JFrame("Personal Information");
+
+        ButtonPanel = new JPanel();
+        frame1.add(ButtonPanel, BorderLayout.SOUTH);
+
+        NameSet = new JButton("Set Name");
+        NameSet.addActionListener(this);
+        ButtonPanel.add(NameSet);
+
+        ProfileSet = new JButton("Set Profile");
+        ProfileSet.addActionListener(this);
+        ButtonPanel.add(ProfileSet);
+
+        ButtonPanel = new JPanel();
+        frame1.add(ButtonPanel, BorderLayout.NORTH);
 
     }
 }

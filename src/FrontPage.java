45c6577fba325bf97e9e3e83a788bb3c847ab8d4
buju <25bwang@athenian.org>
2023-05-10 09:56:10 -0700

    Added a frame, a jpanel and a button

--- src/FrontPage.java
+++ src/FrontPage.java
@@ -1,14 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FrontPage implements ActionListener {
     JFrame frame;
+    JPanel buttonPanel;
+    JButton SocialCircle;
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("SocialCircle")){
 
+        }
     }
     public FrontPage(){
-        frame = new JFrame();
+        frame = new JFrame("Mechat");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        buttonPanel = new JPanel();
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
+        SocialCircle = new JButton("SocialCircle");
+        SocialCircle.addActionListener(this);
+        buttonPanel.add(SocialCircle);
     }
 }

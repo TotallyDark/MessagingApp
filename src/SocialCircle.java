4563c951e48058c218961cbdfa3bed5a0f71292f
buju <25bwang@athenian.org>
2023-05-18 12:55:19 -0700

    Buttons

--- src/SocialCircle.java
+++ src/SocialCircle.java
@@ -4,31 +4,35 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 public class SocialCircle extends JPanel implements ActionListener{
     JTextField textField = new JTextField(20);
     JPanel buttonPanel = new JPanel();
-    JPanel textPage = new JPanel();
+    private JList<String> textMessage;
+    private DefaultListModel<String> listModel;
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
-    public SocialCircle() {
+    public SocialCircle(JFrame frame) {
         setLayout(new BorderLayout());
         add(buttonPanel, BorderLayout.NORTH);
         buttonPanel.add(textField);
         buttonPanel.add(new JButton("Image"));
-        add(textPage, BorderLayout.SOUTH);
+
+        listModel = new DefaultListModel<>();
+        textMessage = new JList<>(listModel);
+        JScrollPane scrollPane = new JScrollPane(textMessage);
+        add(scrollPane, BorderLayout.SOUTH);
         textField.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 if(e.getKeyCode() == KeyEvent.VK_ENTER){
                     String text =textField.getText();
                     textField.setText("");
-                    JLabel text1 = new JLabel(text);
-                    textPage.add(text1);
+                    listModel.addElement(text);
                     repaint();
-                    System.out.println(getSize());
                 }
             }
         });

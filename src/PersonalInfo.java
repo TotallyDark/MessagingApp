d1728772e159365d50491c35e2d621c8a0aaa931
Jerry <25rchen@athenian.org>
2023-05-18 11:29:33 -0700

    Personal Info

--- src/PersonalInfo.java
+++ src/PersonalInfo.java
@@ -6,26 +6,38 @@ public class PersonalInfo extends JPanel implements ActionListener{
     JButton NameSet, ProfileSet, Change;
     JLabel label1, label2;
     JTextField text;
+    JPanel Top, Center;
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equals("NameSet")) {
-            label1 = new JLabel("Current Name: ");
+        if(e.getActionCommand().equals("Set Name")) {
+            Center = new JPanel();
+            Center.setSize(10, 10);
+            label1 = new JLabel("Current Name:");
             label2 = new JLabel("User");
-            this.add(label1);
-            this.add(label2);
-
+            Center.add(label1);
+            Center.add(label2);
+            this.add(Center, BorderLayout.CENTER);
+            this.validate();
+            this.repaint();
         }
     }
     public PersonalInfo() {
+        setLayout(new BorderLayout());
+        Top = new JPanel();
+
         NameSet = new JButton("Set Name");
         NameSet.addActionListener(this);
-        this.add(NameSet);
+        Top.add(NameSet);
 
         ProfileSet = new JButton("Set Profile");
         ProfileSet.addActionListener(this);
-        this.add(ProfileSet);
+        Top.add(ProfileSet);
+
+        this.add(Top, BorderLayout.NORTH);
+        this.validate();
+        this.repaint();
     }
 
 }

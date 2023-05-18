e8bd9fc8198b6f80766ac0ce192748335f3502fa
Jerry <25rchen@athenian.org>
2023-05-18 12:56:48 -0700

    Personal Info

--- src/PersonalInfo.java
+++ src/PersonalInfo.java
@@ -2,23 +2,59 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
+
 public class PersonalInfo extends JPanel implements ActionListener{
     JButton NameSet, ProfileSet, Change;
-    JLabel label1, label2;
+    JLabel label1, label2, label3;
     JTextField text;
-    JPanel Top, Center;
+    JPanel Top, first, second, NameSection;
+    private String name;
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getActionCommand().equals("Set Name")) {
-            Center = new JPanel();
-            Center.setSize(10, 10);
+            NameSection = new JPanel();
+            NameSection.setLayout(new GridLayout(2, 1));
+            first = new JPanel();
+            second = new JPanel();
+
             label1 = new JLabel("Current Name:");
-            label2 = new JLabel("User");
-            Center.add(label1);
-            Center.add(label2);
-            this.add(Center, BorderLayout.CENTER);
+            label2 = new JLabel(name);
+            first.add(label1);
+            first.add(label2);
+
+            label3 = new JLabel("New Name: ");
+            text = new JTextField(15);
+            second.add(label3);
+            second.add(text);
+
+            NameSection.add(first);
+            NameSection.add(second);
+            this.validate();
+            this.repaint();
+            this.add(NameSection, BorderLayout.CENTER);
+        }
+        if(text.getText() != "") {
+            text.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    if(e.getKeyCode() == KeyEvent.VK_ENTER){
+                        SwingUtilities.invokeLater(
+                                new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        label2.setText(text.getText());
+                                        text.setText("");
+                                    }
+                                }
+                        );
+
+                    }
+                }
+            });
             this.validate();
             this.repaint();
         }
@@ -35,9 +71,14 @@ public class PersonalInfo extends JPanel implements ActionListener{
         ProfileSet.addActionListener(this);
         Top.add(ProfileSet);
 
-        this.add(Top, BorderLayout.NORTH);
+        this.name = "User";
+
         this.validate();
         this.repaint();
+        this.add(Top, BorderLayout.NORTH);
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 
 }

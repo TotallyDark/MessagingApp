82c0746c46b35cb13529cbb63f1e987cf2a644cd
Jerry <25rchen@athenian.org>
2023-05-25 08:51:06 -0700

    Pretty much done with the whole personalInfo class, just pushing to keep everyone up to date

--- src/FrontPage.java
+++ src/FrontPage.java
@@ -8,33 +8,37 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class FrontPage implements ActionListener {
+    private String url = "https://as2.ftcdn.net/v2/jpg/00/64/67/63/1000_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg";
     JFrame frame;
     JPanel buttonPanel, pagePanel;
     JButton SocialCircle, MessagePage, AddFriend, PersonalInfo;
+    JLabel profile;
     private String user = "User", URL;
     private JPanel thePanel = new JPanel();
+    private JLabel general = new JLabel();
     private final Dimension PhotoDimension = new Dimension(50, 50);
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equals("SocialCircle")){
+        if (e.getActionCommand().equals("SocialCircle")) {
             pagePanel.remove(thePanel);
             SocialCircle x = new SocialCircle();
             pagePanel.add(x);
             frame.getContentPane().validate();
             frame.getContentPane().repaint();
             thePanel = x;
-        }
-        else if(e.getActionCommand().equals("PersonalInfo")) {
+        } else if (e.getActionCommand().equals("PersonalInfo")) {
             pagePanel.remove(thePanel);
             PersonalInfo y = new PersonalInfo(user, this, URL);
             pagePanel.add(y, BorderLayout.CENTER);
             frame.getContentPane().validate();
             frame.getContentPane().repaint();
             thePanel = y;
-        }
-        else if(e.getActionCommand().equals("MessagePage")) {
+        } else if (e.getActionCommand().equals("MessagePage")) {
             pagePanel.remove(thePanel);
             Server z = new Server(6789);
             pagePanel.add(z, BorderLayout.SOUTH);
@@ -43,6 +47,7 @@ public class FrontPage implements ActionListener {
             z.startRun();
         }
     }
+
     public FrontPage() throws IOException {
         frame = new JFrame("Mechat");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -66,43 +71,55 @@ public class FrontPage implements ActionListener {
         buttonPanel.add(PersonalInfo);
 
         frame.add(buttonPanel, BorderLayout.SOUTH);
+        profile = new JLabel();
+        setImage();
 
-        ImageIcon IIcon = new ImageIcon("Photos/Profile_Picture_Default.png");
-        JLabel image = new JLabel(IIcon);
+        pagePanel = new JPanel();
+        frame.add(pagePanel, BorderLayout.NORTH);
+        pagePanel.setSize(600, 600);
 
-        Image img = ImageIO.read(new File("Photos/Profile_Picture_Default.png"));
+        frame.setSize(600, 600);
+        frame.setVisible(true);
+    }
+
+    public void setImage() throws MalformedURLException {
+        buttonPanel.remove(general);
+        ImageIcon IIcon = new ImageIcon(url);
+        Image img = null;
+        URL url1 = new URL(url);
+        try {
+            img = ImageIO.read(url1);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
         BufferedImage mainIcon = (BufferedImage) img;
         int diameter = Math.min(mainIcon.getHeight(), mainIcon.getWidth());
         BufferedImage mask = new BufferedImage(mainIcon.getWidth(), mainIcon.getHeight(), BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = mask.createGraphics();
 
-        g.fillOval(0,0, diameter-1, diameter-1);
+        g.fillOval(0, 0, diameter - 1, diameter - 1);
         g.dispose();
 
         BufferedImage masked = new BufferedImage(diameter, diameter, BufferedImage.TYPE_INT_ARGB);
         g = masked.createGraphics();
-        int x = (diameter-mainIcon.getWidth()) / 2;
-        int y = (diameter-mainIcon.getHeight()) / 2;
-        g.drawImage(mainIcon,x,y,null);
+        int x = (diameter - mainIcon.getWidth()) / 2;
+        int y = (diameter - mainIcon.getHeight()) / 2;
+        g.drawImage(mainIcon, x, y, null);
         g.setComposite(AlphaComposite.getInstance(AlphaComposite.DST_IN));
-        g.drawImage(mask,0,0,null);
+        g.drawImage(mask, 0, 0, null);
         g.dispose();
 
 
-        IIcon.setImage(masked.getScaledInstance((int)PhotoDimension.getWidth(), (int)PhotoDimension.getHeight(), Image.SCALE_DEFAULT));
-        buttonPanel.add(image);
-        image.setPreferredSize(PhotoDimension);
-
-
-        pagePanel = new JPanel();
-        frame.add(pagePanel, BorderLayout.NORTH);
-        pagePanel.setSize(600,600);
-
-        frame.setSize(600, 600);
-        frame.setVisible(true);
+        IIcon.setImage(masked.getScaledInstance((int) PhotoDimension.getWidth(), (int) PhotoDimension.getHeight(), Image.SCALE_DEFAULT));
+        profile = new JLabel(IIcon);
+        general = profile;
+        buttonPanel.add(general);
+        frame.repaint();
+        frame.revalidate();
     }
+
     public void setName(String name) {
        this.user = name;
     }
-    public void setProfile(String URL) {this.URL = URL;}
+    public void setUrl(String url) {this.url = url;}
 }
